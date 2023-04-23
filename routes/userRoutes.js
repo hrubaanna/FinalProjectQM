ee75e5023fcc9e8b77b3526c67f705e97da59778
Anna Hruba <hruba.skoly@gmail.com>
2023-04-23 21:40:22 +0100

    API work

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
